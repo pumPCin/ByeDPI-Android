dad4b913f4edbdfcb7324302e0b2485706ef92d9
Alex <pumpcin14@mail.ru>
2025-03-05 17:57:59 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 20
-        versionName = "1.5.6"
+        versionName = "1.5.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
