85a450d8f09be6b49c224216c815ceb8adba60f9
Alex <pumpcin14@mail.ru>
2025-07-07 06:40:02 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,8 @@ android {
     defaultConfig {
         applicationId = "io.github.dovecoteescapee.byedpi"
         minSdk = 21
-        //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 1650
+        versionCode = 1651
         versionName = "1.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
