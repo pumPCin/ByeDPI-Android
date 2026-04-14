f211b72fc2aa4fe5a20d9753d2198f300d06a676
pumPCin <pumpcin14@mail.ru>
2026-04-14 20:02:16 +0300

    update

---
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/activities/ToggleActivity.kt
@@ -0,0 +1,105 @@
+package io.github.dovecoteescapee.byedpi.activities
+
+import android.app.Activity
+import android.content.SharedPreferences
+import android.net.VpnService
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.edit
+import io.github.dovecoteescapee.byedpi.data.AppStatus
+import io.github.dovecoteescapee.byedpi.data.Mode
+import io.github.dovecoteescapee.byedpi.services.ServiceManager
+import io.github.dovecoteescapee.byedpi.services.appStatus
+import io.github.dovecoteescapee.byedpi.utility.getPreferences
+import io.github.dovecoteescapee.byedpi.utility.mode
+
+class ToggleActivity : Activity() {
+
+    companion object {
+        private const val TAG = "ToggleServiceActivity"
+    }
+
+    private lateinit var prefs: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        prefs = getPreferences()
+        val strategy = intent.getStringExtra("strategy")
+        val updated = updateStrategy(strategy)
+
+        val onlyUpdate = intent.getBooleanExtra("only_update", false)
+        val onlyStart = intent.getBooleanExtra("only_start", false)
+        val onlyStop = intent.getBooleanExtra("only_stop", false)
+
+        when {
+            onlyStart -> {
+                val (status) = appStatus
+                if (status == AppStatus.Halted) {
+                    startService()
+                }
+            }
+            onlyStop -> {
+                val (status) = appStatus
+                if (status == AppStatus.Running) {
+                    stopService()
+                }
+            }
+            else -> {
+                toggleService(updated)
+            }
+        }
+
+        finish()
+    }
+
+    private fun startService() {
+        val mode = prefs.mode()
+
+        if (mode == Mode.VPN && VpnService.prepare(this) != null) {
+            return
+        }
+
+        ServiceManager.start(this, mode)
+    }
+
+    private fun restartService() {
+        val mode = prefs.mode()
+
+        if (mode == Mode.VPN && VpnService.prepare(this) != null) {
+            return
+        }
+
+        ServiceManager.restart(this, mode)
+    }
+
+    private fun stopService() {
+        ServiceManager.stop(this)
+    }
+
+    private fun toggleService(restart: Boolean) {
+        val (status) = appStatus
+        when (status) {
+            AppStatus.Halted -> {
+                startService()
+            }
+            AppStatus.Running -> {
+                if (restart) {
+                    restartService()
+                } else {
+                    stopService()
+                }
+            }
+        }
+    }
+
+    private fun updateStrategy(strategy: String?): Boolean {
+        val current = prefs.getString("byedpi_cmd_args", null)
+        if (strategy != null && strategy != current) {
+            prefs.edit(commit = true) { putString("byedpi_cmd_args", strategy) }
+            return true
+        }
+        return false
+    }
+}
